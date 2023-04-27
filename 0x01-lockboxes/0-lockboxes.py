d30a6c38fe14b959a3196c4a8553e916cc6d4a69
Martin Agoha <martin4dtruth@gmail.com>
2023-04-27 07:44:35 +0000

    Updated README.md

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,17 @@
+#!/usr/bin/python3
+"""This Script will unlock list of lists"""
+
+
 def canUnlockAll(boxes):
-    # set to keep track of keys we have
-    keys = set([0])
-    # list of boxes that we can open
-    boxes_to_open = [0]
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
 
-    # loop through the boxes that we can open
-    while boxes_to_open:
-        box = boxes_to_open.pop()
-        # loop through the keys in the current box
-        for key in boxes[box]:
-            # if we have not seen this key before, add it to our set of keys
-            if key not in keys:
-                keys.add(key)
-                # add the box that can be opened with this key to our list of boxes to open
-                boxes_to_open.append(key)
-
-    # check if we have opened all the boxes
-    return len(keys) == len(boxes)
-
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
